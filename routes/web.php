4daf3bd96822afef170f16dd8fcdd9f6a76d90f3
田中 <t12100613@yahoo.co.jp>
2021-03-03 10:51:50 +0000

    laravelkadai4

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+  Route::get('admin/profile/create', 'Admin\ProfileController@add');
+  Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
