6015600c804fd097e7b4c4319bcbae8bcf9af945
田中 <t12100613@yahoo.co.jp>
2021-04-20 23:54:08 +0000

    lll

--- app/News.php
+++ app/News.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+  protected $guarded = array('id');
+
+    // 以下を追記
+  public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+    ); //
 }
